fdd1aba5c8c4d3e02b0195761d6e7e64df52d78d
Jarred Taylor <jarrede20@gmail.com>
2022-06-11 23:00:04 -0400

    used the sunrise sunset api to compare those times to the current time

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+my_lat = 32.613003
+my_lng = -83.624199
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameters = {
+    "lat": my_lat,
+    "lng": my_lng,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params = parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+print(sunrise)
+print(sunset)
 
-iss_position = (longitude, latitude)
-
-print(iss_position)
+time_now = datetime.now()
+print(time_now.hour) # I needed the current hour to be displayed so I can know how far I am off from the sunrise and sunset
